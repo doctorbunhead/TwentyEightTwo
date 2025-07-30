75d4f6f32fad050edc43a147fd28e2a1ed9252d0
doctorbunhead <kristian.kd.dunn@gmail.com>
2025-07-30 10:05:41 +0100

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TwentyEightTwo;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        lblTime = new Label();
+        lblState = new Label();
+        btnSkip = new Button();
+        SuspendLayout();
+        // 
+        // lblTime
+        // 
+        lblTime.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        lblTime.AutoSize = true;
+        lblTime.Location = new Point(197, 134);
+        lblTime.Name = "lblTime";
+        lblTime.Size = new Size(194, 25);
+        lblTime.TabIndex = 0;
+        lblTime.Text = "Time Remaining=00:00";
+        // 
+        // lblState
+        // 
+        lblState.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        lblState.AutoSize = true;
+        lblState.Location = new Point(235, 70);
+        lblState.Name = "lblState";
+        lblState.Size = new Size(119, 25);
+        lblState.TabIndex = 1;
+        lblState.Text = "State - Sitting";
+        // 
+        // btnSkip
+        // 
+        btnSkip.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        btnSkip.Location = new Point(235, 188);
+        btnSkip.Name = "btnSkip";
+        btnSkip.Size = new Size(112, 34);
+        btnSkip.TabIndex = 2;
+        btnSkip.Text = "Skip";
+        btnSkip.UseVisualStyleBackColor = true;
+        btnSkip.Click += btnSkip_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(10F, 25F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(588, 234);
+        Controls.Add(btnSkip);
+        Controls.Add(lblState);
+        Controls.Add(lblTime);
+        DoubleBuffered = true;
+        Icon = (Icon)resources.GetObject("$this.Icon");
+        MaximizeBox = false;
+        MaximumSize = new Size(610, 290);
+        MinimumSize = new Size(610, 290);
+        Name = "Form1";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "Twenty Eight Two Reminder";
+        ResizeEnd += Form1_ResizeEnd;
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Label lblTime;
+    private Label lblState;
+    private Button btnSkip;
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace TwentyEightTwo;
+
+public partial class Form1 : Form
+{
+    private Timer timer;
+    private Timer timerDisplay;
+    private DateTime datetimeEnd = DateTime.Now;
+
+    public Form1()
+    {
+        InitializeComponent();
+        timer = new Timer
+        {
+            AutoReset = false,
+            Interval = 20 * 60000 + 10000
+        };
+        this.Shown += form_shown;
+        timer.Elapsed += timer_elapsed;
+
+        timerDisplay = new Timer
+        {
+            AutoReset = true,
+            Interval = 500
+        };
+        timerDisplay.Elapsed += (o, e) =>
+        {
+            var time = TimeSpan.FromMilliseconds(datetimeEnd.TimeOfDay.TotalMilliseconds - DateTime.Now.TimeOfDay.TotalMilliseconds);
+            BeginInvoke((MethodInvoker)delegate () { lblTime.Text = $"Time Remaining={time.ToString("mm\\:ss")}"; lblTime.Left = (ClientSize.Width - lblTime.Width) / 2; });
+        };
+    }
+
+    private void form_shown(Object sender, EventArgs e)
+    {
+        datetimeEnd = DateTime.Now.AddMilliseconds(timer.Interval);
+        timer.Start();
+        timerDisplay.Start();
+    }
+
+    private void timer_elapsed(object sender, ElapsedEventArgs e)
+    {
+        BeginInvoke((MethodInvoker)delegate ()
+        {
+            if (WindowState == FormWindowState.Minimized) WindowState = FormWindowState.Normal;
+            this.TopMost = true;
+            this.CenterToScreen();
+        });
+        var message = "";
+        switch (timer.Interval)
+        {
+            case (20 * 60000):
+                timer.Interval = 8 * 60000 + 10000;
+                message = "Standing Up";
+                break;
+
+            case (8 * 60000):
+                timer.Interval = 2 * 60000 + 10000;
+                message = "Moving Around";
+                break;
+
+            case (2 * 60000):
+                timer.Interval = 20 * 60000 + 10000;
+                message = "Sitting";
+                break;
+        }
+        datetimeEnd = DateTime.Now.AddMilliseconds(timer.Interval);
+        timer.Start();
+        BeginInvoke((MethodInvoker)delegate ()
+        {
+            TopMost = false; lblState.Text = $"State - {message}";
+            lblState.Left = (ClientSize.Width - lblState.Width) / 2;
+        });
+        Console.Beep(500, 200);
+        Thread.Sleep(200);
+        Console.Beep(500, 200);
+        Thread.Sleep(200);
+        Console.Beep(500, 200);
+    }
+
+    private void Form1_ResizeEnd(object sender, EventArgs e)
+    {
+        Refresh();
+    }
+
+    private void btnSkip_Click(object sender, EventArgs e)
+    {
+        timer.Stop();
+        timer_elapsed(null, null);
+    }
+}
